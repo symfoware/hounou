1ad32ed0b68c670705756868d60800d7aeaeea43
symfoware <symfoware@outlook.com>
2024-05-27 19:19:35 +0900

    make task

--- index.js
+++ index.js
@@ -1,41 +1,58 @@
 import { ArgumentParser } from 'argparse';
-
-const parser = new ArgumentParser({
-    description: 'Argparse example'
-  });
-//parser.add_argument('deploy');
-parser.add_argument('--region');
-parser.add_argument('--accessKeyId');
-parser.add_argument('--secretAccessKey');
-const args = parser.parse_args();
-
-
-
 import { Config } from './lib/config.js';
 import { Lambda } from './lib/lambda.js';
 
-try {
-    const config = new Config(args);
-    const lambda = new Lambda(config);
-    // 指定された関数情報取得
-    //await lambda.getFunctionsInfo();
+const main = async () => {
+    const parser = new ArgumentParser({
+        description: 'Argparse example'
+      });
+    //parser.add_argument('deploy');
+    parser.add_argument('--region');
+    parser.add_argument('--accessKeyId');
+    parser.add_argument('--secretAccessKey');
+    const args = parser.parse_args();
     
-    // レイヤー作成
-    //await lambda.createLayers();
+    try {
+        const config = new Config(args);
+        const lambda = new Lambda(config);
 
-    // レイヤー情報取得
-    //await lambda.getLayer();
-    //await lambda.test();
+        // 処理フロー
+        // 変更が必要な内容を確定
+        await lambda.makeTask();
 
-    // 本体関数更新
-    //await lambda.update();
-    // バージョン発行
-    await lambda.publishVersion();
+        // 変更実施
+        // バージョン発行
+
+        // 不要なバージョン、レイヤーを削除
+
+
+
+
+
+
+
+        // 指定された関数情報取得
+        //await lambda.getFunctionsInfo();
+        
+        // レイヤー作成
+        //await lambda.createLayers();
     
-} catch(e) {
-    console.error(e.message);
-}
+        // レイヤー情報取得
+        //await lambda.getLayer();
+        //await lambda.test();
+    
+        // 本体関数更新
+        //await lambda.update();
+        // バージョン発行
+        //await lambda.publishVersion();
+        
+    } catch(e) {
+        console.error(e.message);
+        console.error(e);
+    }
+
+};
 
 
-
+export default main;
 
